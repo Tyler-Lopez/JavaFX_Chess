96a59e95bfd89ecce3c63a00e66bc3d6872c4f0a
Tyler-Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2021-07-09 14:52:36 -0400

    Addn. of javaFX lib

--- Chess/src/sample/Main.java
+++ Chess/src/sample/Main.java
@@ -1,21 +1,7 @@
 package sample;
 
-import javafx.animation.Animation;
-import javafx.animation.AnimationTimer;
-import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Main extends Application {
 
